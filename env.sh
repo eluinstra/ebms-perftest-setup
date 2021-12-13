35326fa35e6120ea5201dae6b35c3d248efb5fb0
John Wentink <john.wentink@gmail.com>
2021-12-13 23:10:24 +0100

    script fixes

--- env.sh
+++ env.sh
@@ -2,10 +2,10 @@ export OVERHEID_HOME=$HOME/overheid
 export DIGIPOORT_HOME=$HOME/digipoort
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export JDBCJAR=postgresql-42.2.14.jar
+export JDBCJAR=postgresql-42.2.23.jar
 export LOG4J_CONFIGURATION_FILE=$HOME/lib/log4j2.xml
 
-export VERSION=2.18.2
+export VERSION=2.18.4
 
 export JAVA_ARGS="-Xms2048m -Xmx2048m"
 
--- setup-testenv.sh
+++ setup-testenv.sh
@@ -16,7 +16,11 @@ fi
 # download stuff
 curl -O https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.4.1.tgz
 curl https://repo1.maven.org/maven2/org/postgresql/postgresql/42.2.23/postgresql-42.2.23.jar -o lib/postgresql-42.2.23.jar
+curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin.2.18.4/ebms-admin-2.18.4.jar -o lib/ebms-admin-2.18.4.jar
+curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin.2.18.3/ebms-admin-2.18.3.jar -o lib/ebms-admin-2.18.3.jar
 curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin.2.18.2/ebms-admin-2.18.2.jar -o lib/ebms-admin-2.18.2.jar
 curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin.2.18.1/ebms-admin-2.18.1.jar -o lib/ebms-admin-2.18.1.jar
+curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin.2.17.10/ebms-admin-2.17.10.jar -o lib/ebms-admin-2.17.10.jar
+curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin.2.17.9/ebms-admin-2.17.9.jar -o lib/ebms-admin-2.17.9.jar
 curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin.2.17.8/ebms-admin-2.17.8.jar -o lib/ebms-admin-2.17.8.jar
 curl -L https://github.com/eluinstra/ebms-admin/releases/download/ebms-admin-2.17.7/ebms-admin-2.17.7.jar -o lib/ebms-admin-2.17.7.jar
